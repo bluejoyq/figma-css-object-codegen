87844bc84769cdaface72f5121527da00a960521
bluejoyq <bluejoy32@gmail.com>
2023-07-26 02:33:09 +0900

    init

---
+++ code.ts
@@ -0,0 +1,36 @@
+type CSS ={
+  [key: string]: string;
+}
+const kebabToCamelCase = (str: string): string => str.replace(/-([a-z])/g, (_match, char) => char.toUpperCase());
+const addQuotesIfNeeded = (value: string): string => {
+  return isNaN(parseFloat(value)) || !isFinite(value as any) ? `"${value}"` : value;
+};
+const cssObjectToCssString = (cssObject:CSS): string => {
+  let result = '{\n';
+
+  for (const property of Object.keys(cssObject)) {
+    const value = cssObject[property];
+    const camelCaseProperty = kebabToCamelCase(property);
+    const quotedValue = addQuotesIfNeeded(value);
+    result += `\t${camelCaseProperty}: ${quotedValue};\n`;
+  }
+
+  return result + '\n}';
+};
+
+const genCssStr = async (node : SceneNode ) : Promise<string> => {
+  const css = await node.getCSSAsync();
+  return cssObjectToCssString(css);
+}
+
+figma.codegen.on('generate', async (e : CodegenEvent) => {
+  const node = e.node;
+
+  const parentCss = await genCssStr(node);
+
+  return  [{
+    title: 'css object',
+    code: parentCss,
+    language: 'JAVASCRIPT'
+  }];
+});
